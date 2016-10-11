d8a7b0f28015991c0e90e375fc5e8c21f5a757be
Sergio García Domínguez <sergio_gd92@hotmail.com>
2016-10-11 09:58:08 +0200

    Actualizados ejercicios 18, 19 y 20

--- Ejercicios_Capitulo_4/Ejercicio18.java
+++ Ejercicios_Capitulo_4/Ejercicio18.java
@@ -9,17 +9,21 @@
 public class Ejercicio18 {
   public static void main (String[] args) {
     System.out.print("Introduzca un número entero (máximo 5 cifras): ");
-    String texto = System.console().readLine();
-    int numero = Integer.parseInt(texto);
+    int numero = Integer.parseInt(System.console().readLine());
+    int primeraCifra = numero;
     
-    if ((texto.length() <= 5) && (numero >= 0)) {
-      System.out.println("La primera cifra del número introducido es: " + texto.charAt(0));
+    if (numero > -10 && numero < 10) {
       
-    } else if ((texto.length() <= 6) && (numero < 0)) {
-      System.out.println("La primera cifra del número introducido es: " + texto.charAt(1));
-      
-    } else {
-      System.out.println("El número introducido ha de tener 5 cifras como máximo.");
+    } else if (numero > -100 && numero < 100) {
+      primeraCifra = (numero / 10);
+    } else if (numero > -1000 && numero < 1000) {
+      primeraCifra = (numero / 100);
+    } else if (numero > -10000 && numero < 10000) {
+      primeraCifra = (numero / 1000);
+    } else if (numero > -100000 && numero < 100000) {
+      primeraCifra = (numero / 10000);
     }
+    
+    System.out.println("\nLa primera cifra es: " + primeraCifra);
   }
 }
