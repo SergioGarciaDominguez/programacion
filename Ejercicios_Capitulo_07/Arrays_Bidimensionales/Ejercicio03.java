3d6f66f910c00fbef75fc127ccb8f6124b0e4518
Sergio García Domínguez <sergio_gd92@hotmail.com>
2017-01-17 08:30:57 +0100

    Añadididos ejercicios funciones

---
+++ Ejercicios_Capitulo_07/Arrays_Bidimensionales/Ejercicio03.java
@@ -0,0 +1,68 @@
+/**
+* Capítulo 7 - Arrays Bidimensionales - Ejercicio 03
+*
+* Modifica el programa anterior de tal forma que los números que se introducen en el array se
+* generen de forma aleatoria (valores entre 100 y 999).
+*
+* @author Sergio García Domínguez
+*/
+
+public class Ejercicio03 {
+  public static void main (String[] args) {
+
+    int[][] array = new int[4][5];
+    int i;
+    int j;
+    int sumaFilas = 0;
+    int sumaColumnas = 0;
+    int sumaTotal = 0;
+
+    System.out.println("Vamos a hacer un array bidimensional de números aleatorio de 4 filas y 5 columnas");
+
+    for (i = 0; i < 4; i++) {
+
+      for (j = 0; j < 5; j++) {
+
+        array[i][j] = (int)(Math.random() * 900 + 101);
+      }
+    }
+
+    System.out.println("┌─────────┬────────┬────────┬────────┬────────┬────────┬────────┐");
+    System.out.println("│ Índice  │Columna0│Columna1│Columna2│Columna3│Columna4│  Suma  │ ");
+
+
+    for (i = 0; i < 4; i++) {
+      System.out.println("├─────────┼────────┼────────┼────────┼────────┼────────┼────────┤");
+
+      System.out.print("│ Fila" + i + "   ");
+
+      for (j = 0; j < 5; j++) {
+        System.out.printf("│ %-7d", array[i][j]);
+        sumaFilas += array[i][j];
+        }
+
+      System.out.printf("│ %-7d", sumaFilas);
+      System.out.println("│");
+      sumaTotal += sumaFilas;
+      sumaFilas = 0;
+
+    }
+    System.out.println("├─────────┼────────┼────────┼────────┼────────┼────────┼────────┤");
+
+    System.out.print("│ Suma    ");
+
+    for (j = 0; j < 5; j++) {
+
+      for (i = 0; i < 4; i++) {
+        sumaColumnas += array[i][j];
+      }
+      System.out.printf("│ %-7d", sumaColumnas);
+      sumaColumnas = 0;
+    }
+    System.out.printf("│ %-7d", sumaTotal);
+    System.out.print("│");
+
+    System.out.println();
+    System.out.println("└─────────┴────────┴────────┴────────┴────────┴────────┴────────┘");
+  }
+}
---
+++ Ejercicios_Capitulo_08/Ejercicio19.java
@@ -0,0 +1,181 @@
+/**
+ * Biblioteca
+ * @author Sergio García Domínguez
+ */
+import java.util.Scanner;
+
+public class Ejercicio19 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Vamos a cambiar un número de base\n");
+    System.out.print("Introduzca el número a cambiar: ");
+    String numero = s.nextLine();
+    System.out.println("\nSeleccione la base de partida (introduzca el número del menú):");
+    System.out.println("2. Binario");
+    System.out.println("8. Octal");
+    System.out.println("10. Decimal");
+    System.out.println("16. Hexadecimal");
+    int basePartida = Integer.parseInt(s.nextLine());
+    System.out.println("\nSeleccione la base a la que cambiar (introduzca el número del menú):");
+    System.out.println("2. Binario");
+    System.out.println("8. Octal");
+    System.out.println("10. Decimal");
+    System.out.println("16. Hexadecimal");
+    int baseDeseada = Integer.parseInt(s.nextLine());
+    System.out.print("\nEl número en base " + baseDeseada + " es el ");
+    
+    if (basePartida != baseDeseada) {
+      int numeroDecimal;
+      
+      switch (basePartida) {
+        case 2:
+        case 8:
+          numeroDecimal = cambiarADecimal(Integer.parseInt(numero), basePartida);  
+          break;
+        case 16:
+          numeroDecimal = hexadecimalADecimal(numero);
+          break;
+        default:
+          numeroDecimal = Integer.parseInt(numero);
+          break;
+      }
+
+      switch (baseDeseada) {
+        case 2:
+        case 8:
+          System.out.println(decimalABase(numeroDecimal, baseDeseada));
+          break;
+        case 10:
+          System.out.println(numeroDecimal);
+          break;
+        case 16:
+          System.out.println(decimalAHexadecimal(numeroDecimal));
+          break;
+      }
+    
+    } else {
+      System.out.println(Integer.parseInt(numero));
+    }
+    
+  }
+  
+  public static int decimalABase(int n, int b) {
+    int numeroCambiado = 0;
+    int num = n;
+    
+    for (int i = 1; i <= (int)(Math.log(n) / Math.log(b)) + 1; i++) {
+      numeroCambiado += ((int)(Math.pow(10, i - 1) * (num % b)));
+      num /= b;
+    }
+    
+    return numeroCambiado;
+  }
+  
+  public static int cambiarADecimal(int n, int b) {
+    int numeroCambiado = 0;
+    
+    for (int i = 1; i <= digitos(n); i++) {
+      numeroCambiado += (int)Math.pow(b, i - 1)
+          * (int)((n % Math.pow(10, i)) / Math.pow(10, i - 1));
+    }
+  
+    return numeroCambiado;
+  }
+  
+  public static String decimalAHexadecimal(int n) {
+    String numeroCambiado = "";
+    String[] digitos = new String[(int)(Math.log(n) / Math.log(16)) + 1];
+    
+    for (int i = digitos.length - 1; i >= 0; i--) {
+      
+      switch (n % 16) {
+        case 10:
+          digitos[i] = "A";
+          break;
+        case 11:
+          digitos[i] = "B";
+          break;
+        case 12:
+          digitos[i] = "C";
+          break;
+        case 13:
+          digitos[i] = "D";
+          break;
+        case 14:
+          digitos[i] = "E";
+          break;
+        case 15:
+          digitos[i] = "F";
+          break;
+        default:
+          digitos[i] = String.valueOf(n % 16);
+          break;
+      }
+      
+      n /= 16;
+    }
+    
+    for (String digito : digitos) {
+      numeroCambiado += digito;
+    }
+    
+    return numeroCambiado;
+  }
+  
+  public static int hexadecimalADecimal(String n) {
+    int numeroCambiado = 0;
+    
+    for (int i = n.length() - 1; i >= 0; i--) {
+
+      switch (n.charAt(n.length() - i - 1)) {
+        case 'a':
+        case 'A':
+          numeroCambiado += ((int)Math.pow(16, i) * 10);
+          break;
+        case 'b':
+        case 'B':
+          numeroCambiado += ((int)Math.pow(16, i) * 11);
+          break;
+        case 'c':
+        case 'C':
+          numeroCambiado += ((int)Math.pow(16, i) * 12);
+          break;
+        case 'd':
+        case 'D':
+          numeroCambiado += ((int)Math.pow(16, i) * 13);
+          break;
+        case 'e':
+        case 'E':
+          numeroCambiado += ((int)Math.pow(16, i) * 14);
+          break;
+        case 'f':
+        case 'F':
+          numeroCambiado += ((int)Math.pow(16, i) * 15);
+          break;
+        default:
+          numeroCambiado += (int)Math.pow(16, i)
+              * Integer.parseInt(String.valueOf(n.charAt(n.length() - i - 1)));
+          break;
+      }
+      
+    }
+  
+    return numeroCambiado;
+  }
+  
+  public static int digitos(int n) {
+    int totalDigitos = 0;
+    
+    for (int i = 20; i > 0; i--) {
+      
+      if (n > - Math.pow(10, i) && n < Math.pow(10, i)) {
+        totalDigitos = i;
+      }
+      
+    }
+    
+    return totalDigitos;
+  }
+
+
+}
